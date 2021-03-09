e6416f13359b1843bf9df9f068be8c5a39e6270b
ajahemmanuelchuku@gmail.com <arcnecttech@gmail.com>
2021-03-10 02:21:13 +0800

    Void 1

--- WhatsappClone/navigation/index.tsx
+++ WhatsappClone/navigation/index.tsx
@@ -31,6 +31,9 @@ function RootNavigator() {
     <Stack.Navigator screenOptions={{
         headerStyle: {
           backgroundColor: Colors.light.tint,
+          shadowOpacity: 0,
+          elevation: 0,
+          borderBottomWidth: 0,
         },
         headerTintColor: Colors.light.background,
         headerTitleAlign: "left",
