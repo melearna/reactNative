9fb7e4c54fadce9d4ebfd0455f85032958f406ba
ajahemmanuelchuku@gmail.com <arcnecttech@gmail.com>
2021-03-10 02:37:04 +0800

    Initial Upload 2nd Try

--- WhatsappClone/navigation/BottomTabNavigator.tsx
+++ WhatsappClone/navigation/BottomTabNavigator.tsx
@@ -20,44 +20,44 @@ const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 >>>>>>> parent of 2b80ce0 (Tabs Created):WhatsappClone/navigation/BottomTabNavigator.tsx
 
 export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <MainTab.Navigator
-      initialRouteName="TabOne"
-      tabBarOptions={{
-          activeTintColor: Colors[colorScheme].background,
-          style: {
-              backgroundColor: Colors[colorScheme].tint,
-          },
-          indicatorStyle: {
-              backgroundColor: Colors[colorScheme].background,
-              height: 4,
-          }
+    return (
+        <MainTab.Navigator
+            initialRouteName="TabOne"
+            tabBarOptions={{
+                activeTintColor: Colors[colorScheme].background,
+                style: {
+                    backgroundColor: Colors[colorScheme].tint,
+                },
+                indicatorStyle: {
+                    backgroundColor: Colors[colorScheme].background,
+                    height: 4,
+                }
 
-      }}>
-      <MainTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
-      />
-      <MainTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
-      />
-    </MainTab.Navigator>
-  );
+            }}>
+            <MainTab.Screen
+                name="TabOne"
+                component={TabOneNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+                }}
+            />
+            <MainTab.Screen
+                name="TabTwo"
+                component={TabTwoNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+                }}
+            />
+        </MainTab.Navigator>
+    );
 }
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
 function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+    return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
@@ -65,27 +65,27 @@ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']
 const TabOneStack = createStackNavigator<TabOneParamList>();
 
 function TabOneNavigator() {
-  return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
-      />
-    </TabOneStack.Navigator>
-  );
+    return (
+        <TabOneStack.Navigator>
+            <TabOneStack.Screen
+                name="TabOneScreen"
+                component={TabOneScreen}
+                options={{ headerTitle: 'Tab One Title' }}
+            />
+        </TabOneStack.Navigator>
+    );
 }
 
 const TabTwoStack = createStackNavigator<TabTwoParamList>();
 
 function TabTwoNavigator() {
-  return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
-      />
-    </TabTwoStack.Navigator>
-  );
+    return (
+        <TabTwoStack.Navigator>
+            <TabTwoStack.Screen
+                name="TabTwoScreen"
+                component={TabTwoScreen}
+                options={{ headerTitle: 'Tab Two Title' }}
+            />
+        </TabTwoStack.Navigator>
+    );
 }
--- WhatsappClone/navigation/index.tsx
+++ WhatsappClone/navigation/index.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import {ColorSchemeName, View} from 'react-native';
+import {Octicons, MaterialCommunityIcons} from '@expo/vector-icons';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import Colors from "../constants/Colors";
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -26,7 +28,6 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-<<<<<<< HEAD
     <Stack.Navigator screenOptions={{
         headerStyle: {
           backgroundColor: Colors.light.tint,
@@ -58,10 +59,6 @@ function RootNavigator() {
               )
           }}
       />
-=======
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
->>>>>>> parent of 2b80ce0 (Tabs Created)
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
